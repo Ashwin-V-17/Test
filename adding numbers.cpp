f57314c69efcb8fcf1b1822958595db452808a32
Ashwin-V-17 <103203952+Ashwin-V-17@users.noreply.github.com>
2022-04-07 20:57:34 +0530

    initial commit

---
+++ adding numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,total;
+    cout<<"Enter 2 Numbers: "<<endl;
+    cin>>a>>b;
+    total=a+b;
+    cout<<"The sum of two numbers: "<<total<<endl;
+    return 0;
+}
